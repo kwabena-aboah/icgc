d87fcf8aa074ccdf53971885b518a1c66d29304f
kwabena-aboah <obed30mintah@yahoo.com>
2017-05-12 19:57:55 +0000

    Add files via upload

---
+++ database (1).sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 12, 2017 at 07:55 PM
+-- Server version: 5.5.55-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` smallint(6) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `position` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`, `position`) VALUES
+(2, 'mike', 'for test', 1),
+(3, 'ghjlk,', 'iuuhnl', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pm`
+--
+
+CREATE TABLE IF NOT EXISTS `pm` (
+  `id` bigint(20) NOT NULL,
+  `id2` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `user1` bigint(20) NOT NULL,
+  `user2` bigint(20) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` int(10) NOT NULL,
+  `user1read` varchar(3) NOT NULL,
+  `user2read` varchar(3) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pm`
+--
+
+INSERT INTO `pm` (`id`, `id2`, `title`, `user1`, `user2`, `message`, `timestamp`, `user1read`, `user2read`) VALUES
+(1, 1, 'test', 2, 1, 'just for test', 1490467248, 'yes', 'yes'),
+(2, 1, 'dame', 1, 2, 'got test msg', 1490998668, 'yes', 'yes'),
+(2, 2, '', 2, 0, 'okay', 1490998895, '', ''),
+(2, 3, '', 1, 0, 'leave<br />\r\n', 1490998970, '', ''),
+(5, 1, 'sss', 2, 1, 'this is not me', 1491905460, 'yes', 'yes'),
+(6, 1, 'go', 1, 2, 'uighskdjik', 1494616416, 'yes', 'yes'),
+(6, 2, '', 1, 0, 'ughbdfkhkj', 1494616441, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topics`
+--
+
+CREATE TABLE IF NOT EXISTS `topics` (
+  `parent` smallint(6) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id2` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `message` longtext NOT NULL,
+  `authorid` int(11) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `timestamp2` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`id2`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `topics`
+--
+
+INSERT INTO `topics` (`parent`, `id`, `id2`, `title`, `message`, `authorid`, `timestamp`, `timestamp2`) VALUES
+(2, 1, 1, 'Yep Yep', 'Holla!!!!', 1, 1494616322, 1494616322);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `avatar` text NOT NULL,
+  `signup_date` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `avatar`, `signup_date`) VALUES
+(1, 'admin', '20eabe5d64b0e216796e834f52d61fd0b70332fc', 'obed30mintah@yahoo.com', 'brainstien', 1490297717),
+(2, 'obed', 'b61cd302b61c6800efff1706377782983180c8ad', 'mintahkwabena13@gmail.com', '', 1490467195);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
